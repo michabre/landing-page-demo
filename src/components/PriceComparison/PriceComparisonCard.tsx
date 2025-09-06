198495b6059f779f9d6d1b3e6c0fff296886ab64
Michael Breuer <michael.breuer@aspirecreative.co.uk>
2025-09-06 23:01:07 +0800

    removed nextui from card

--- src/components/PriceComparison/PriceComparisonCard.tsx
+++ src/components/PriceComparison/PriceComparisonCard.tsx
@@ -1,6 +1,9 @@
 import type { IPricingComparisonCard } from '@interfaces/IPricingComparisonCard'
-import {Button, Card, CardHeader, CardBody, CardFooter, Divider} from "@nextui-org/react";
-import "./priceComparisonCard.css";
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Divider from '@mui/material/Divider';
 
 export default function PriceComparisonCard({type, title, description, price, features, selected}:IPricingComparisonCard) {
   const selectedClass = (selected === "true") ? "selected" : "";
@@ -9,9 +12,9 @@ export default function PriceComparisonCard({type, title, description, price, fe
   return (
     <div className='relative h-[100%]'>
        {showTag && <div className='card-tag absolute flex justify-center'><span className='bg-sky-600 px-8 py-1 uppercase rounded-md text-white font-semibold tracking-widest'>Popular</span></div>}
-    <Card className={selectedClass + ' h-[100%]'}>
+    <Card className={selectedClass + ' h-[100%]'} sx={{ padding: '1.5rem', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
      
-      <CardHeader className="flex flex-col justify-center gap-4 md:items-start">
+      <div className="flex flex-col justify-center gap-4 md:items-start">
         <span className='card-type font-bold'>{type}</span>
         <h3 className='text-xl font-bold'>{title}</h3>
         <p className='text-base'>{description}</p>
@@ -20,23 +23,23 @@ export default function PriceComparisonCard({type, title, description, price, fe
           <span className='text-base'>/ month</span>
         </div>
         <p className='text-base italic'>Billed yearly</p>
-      </CardHeader>
+      </div>
       <Divider/>
       <div className='p-3'>
-        <Button color="primary" fullWidth={true} size="lg">Start Free Trial</Button>
+        <Button color="primary" fullWidth={true} size="large">Start Free Trial</Button>
       </div>
       
       <Divider/>
-      <CardBody>
+      <CardContent>
         <h4 className='font-semibold'>What's included:</h4>
         <ul>
           {features.map((feature, index) => <li key={index}>{feature}</li>)}
         </ul>
-      </CardBody>
+      </CardContent>
+
+      <CardActions>
       
-      <CardFooter>
-      
-      </CardFooter>
+      </CardActions>
      
     </Card>
     </div>
